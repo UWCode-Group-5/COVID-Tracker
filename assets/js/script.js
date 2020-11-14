f2922254195a2746e71f465d0f24ca47d290bbe0
Abdulhakeem Dahir <somlutions@gmail.com>
2020-11-13 22:23:43 -0800

    setup both current and historic covid apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Setup Covid Tracker API
 var stateSubmit = $("#state-submit");
 
+        // Empty values
+        var container = $("#container");
+        var date = $("<h3>").text("");
+        var state = $("<h3>").text("");
+        var death = $("<h3>").text("");
+        var hospitalized = $("<h3>").text("");
+        var positive = $("<h3>").text("");
+        var negative = $("<h3>").text("");
+        var positiveIncrease = $("<h3>").text("");
+        var negativeIncrease = $("<h3>").text("");
+        var dataQualityGrade = $("<h3>").text("");
 
 stateSubmit.on("click", function(event){
     event.preventDefault();
@@ -12,17 +23,37 @@ stateSubmit.on("click", function(event){
       method: "GET"
     }).then(function(response) {
         console.log(response);
-        var container = $("#container");
-        var date = $("<h3>").text("Date: " + response.date);
-        var state = $("<h3>").text("State: " + response.state);
-        var death = $("<h3>").text("Death: " + response.death);
-        var hospitalized = $("<h3>").text("Hospitalized: " + response.hospitalized);
-        var positive = $("<h3>").text("Positive: " + response.positive);
-        var negative = $("<h3>").text("Negative: " + response.negative);
-        var positiveIncrease = $("<h3>").text("Positive Increse: " + response.positiveIncrease);
-        var negativeIncrease = $("<h3>").text("Negative Increase: " + response.negativeIncrease);
-        var dataQualityGrade = $("<h3>").text("Data Qualtiy Grade: " + response.dataQualityGrade);
 
+
+        // Searched Values
+        date = $("<h3>").text("Date: " + response.date);
+        state = $("<h3>").text("State: " + response.state);
+        death = $("<h3>").text("Death: " + response.death);
+        hospitalized = $("<h3>").text("Hospitalized: " + response.hospitalized);
+        positive = $("<h3>").text("Positive: " + response.positive);
+        negative = $("<h3>").text("Negative: " + response.negative);
+        positiveIncrease = $("<h3>").text("Positive Increse: " + response.positiveIncrease);
+        negativeIncrease = $("<h3>").text("Negative Increase: " + response.negativeIncrease);
+        dataQualityGrade = $("<h3>").text("Data Qualtiy Grade: " + response.dataQualityGrade);
         container.append(date,state,death,hospitalized,positive,negative,positiveIncrease,negativeIncrease,dataQualityGrade);
+        //Ajax call for historic values
+        var dates = $("#user-date").val();
+        var queryURL = "https://api.covidtracking.com/v1/states/" + states +"/"+ dates +".json";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+        date = $("<h3>").text("Date: " + response.date);
+        state = $("<h3>").text("State: " + response.state);
+        death = $("<h3>").text("Death: " + response.death);
+        hospitalized = $("<h3>").text("Hospitalized: " + response.hospitalized);
+        positive = $("<h3>").text("Positive: " + response.positive);
+        negative = $("<h3>").text("Negative: " + response.negative);
+        positiveIncrease = $("<h3>").text("Positive Increse: " + response.positiveIncrease);
+        negativeIncrease = $("<h3>").text("Negative Increase: " + response.negativeIncrease);
+        dataQualityGrade = $("<h3>").text("Data Qualtiy Grade: " + response.dataQualityGrade);
+        container.append(date,state,death,hospitalized,positive,negative,positiveIncrease,negativeIncrease,dataQualityGrade);
+        });
+    
     });
 });
